504e0395050f574274d89818ccff810dfd4818b9
Sanket2055 <sanketdiwate95@gmail.com>
2021-08-09 16:23:01 +0530

    adding pages

---
+++ src/components/search/search-box.components.jsx
@@ -0,0 +1,13 @@
+import { placeholder } from "@babel/types";
+import React from "react";
+import "./search-box.styles.css"
+export const  SearchBox=({placeholder , handleChange})=>{
+    return(
+        <input type="search"
+        className ="search"
+        placeholder={placeholder}
+        onChange={handleChange}/>
+
+    );
+
+}
